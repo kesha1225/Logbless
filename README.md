7c8311ad881fd218cdf479f6fe61e0223520c590
kesha1225 <samedov03@mail.ru>
2025-01-19 05:40:26 +0300

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# JustLogs
+
+
+_log_format = "%(asctime)s - [%(levelname)s] - %(name)s - (%(filename)s).%(funcName)s(%(lineno)d) - %(message)s"
---
+++ justlogs/constants.py
@@ -0,0 +1,11 @@
+CONFIG_FILENAME = "JL_conf.yaml"
+
+
+BASE_CONFIG = {
+    "host": "127.0.0.1",
+    "port": 8070,
+    "path": "/logs",
+    "log_filename": "logs.log",
+    "title": "JustLogs Log viewer",
+    "authentication": {"login": "admin", "password": "admin"},
+}
